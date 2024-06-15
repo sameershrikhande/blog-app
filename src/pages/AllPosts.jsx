05a204fa954b63f6136a9fcb9d440f50ae685e2f
sameershrikhande <sameershrikhande5@gmail.com>
2024-06-15 13:22:59 +0530

    minor fix

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -22,7 +22,7 @@ function AllPosts() {
 
   return (
     <div className='w-full py-8'>
-      <Container>
+      {/* <Container>
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
@@ -30,7 +30,18 @@ function AllPosts() {
             </div>
           ))}
         </div>
+      </Container> */}
+
+<Container>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
+        </div>
       </Container>
+
     </div>
   )
 }
