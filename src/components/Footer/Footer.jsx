30b7108687eab1fedb523e86b47d49fa79057b92
sameershrikhande <sameershrikhande5@gmail.com>
2024-06-15 13:16:47 +0530

    minor fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer() {
                             </div>
                             <div>
                                 <p className="text-sm text-gray-600">
-                                    &copy; Copyright 2023. All Rights Reserved by BlogApp.
+                                    &copy; Copyright 2024. All Rights Reserved by BlogApp.
                                 </p>
                             </div>
                         </div>
